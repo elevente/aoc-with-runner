dbc133f2aea165a9a73c2323ffb33ab9a94a505a
Levente Szabo <levente.szabo@betssongroup.com>
2025-12-11 10:21:29 +0100

    day 8b

--- aoc2025/README.md
+++ aoc2025/README.md
@@ -18,7 +18,7 @@
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
-[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%86/yellow)](src/day08)
+[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
@@ -118,9 +118,9 @@ Both parts: 194.918ms
 
 ```
 Day 08
-Time part 1: 620.971ms
-Time part 2: -
-Both parts: 620.971ms
+Time part 1: 407.492ms
+Time part 2: 1012.24ms
+Both parts: 1419.732ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 15/50
-Total time: 2269.334ms
+Total stars: 16/50
+Total time: 3068.095ms
 ```
 
 <!--/RESULTS-->
--- aoc2025/src/day08/index.ts
+++ aoc2025/src/day08/index.ts
@@ -1,17 +1,25 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput.split("\n").map(l => l.split(",").map(Number));
+const parseInput = (rawInput: string) =>
+	rawInput.split("\n").map((l) => l.split(",").map(Number));
 
 const distance = (coord1: number[], coord2: number[]) => {
-	return ((coord1[0] - coord2[0]) ** 2) + ((coord1[1] - coord2[1]) ** 2) + ((coord1[2] - coord2[2]) ** 2);
-}
+	return (
+		(coord1[0] - coord2[0]) ** 2 +
+		(coord1[1] - coord2[1]) ** 2 +
+		(coord1[2] - coord2[2]) ** 2
+	);
+};
 
 const part1 = (rawInput: string) => {
 	const input = parseInput(rawInput);
-	let distances: { coords: number[], distance: number }[] = [];
+	let distances: { coords: number[]; distance: number }[] = [];
 	for (let x = 0; x < input.length; x++) {
 		for (let y = x + 1; y < input.length; y++) {
-			distances.push({ coords: [x, y], distance: distance(input[x], input[y]) });
+			distances.push({
+				coords: [x, y],
+				distance: distance(input[x], input[y]),
+			});
 		}
 	}
 	distances.sort((a, b) => a.distance - b.distance);
@@ -21,24 +29,34 @@ const part1 = (rawInput: string) => {
 		return;
 	}
 	for (let i = 0; i < 1000; i++) {
-		const found = circuits.filter(circuit => circuit.includes(distances[i].coords[0]) || circuit.includes(distances[i].coords[1]));
+		const found = circuits.filter(
+			(circuit) =>
+				circuit.includes(distances[i].coords[0]) ||
+				circuit.includes(distances[i].coords[1]),
+		);
 		if (found.length === 0) {
 			circuits.push(distances[i].coords);
 		}
 		if (found.length === 1) {
-			const index = circuits.findIndex(circuit => circuit.join(",") === found[0].join(","));
+			const index = circuits.findIndex(
+				(circuit) => circuit.join(",") === found[0].join(","),
+			);
 			circuits[index].push(...distances[i].coords);
 		}
 		if (found.length === 2) {
-			const index1 = circuits.findIndex(circuit => circuit.join(",") === found[0].join(","));
-			const index2 = circuits.findIndex(circuit => circuit.join(",") === found[1].join(","));
+			const index1 = circuits.findIndex(
+				(circuit) => circuit.join(",") === found[0].join(","),
+			);
+			const index2 = circuits.findIndex(
+				(circuit) => circuit.join(",") === found[1].join(","),
+			);
 			circuits[index1].push(...circuits[index2]);
 			circuits[index1].push(...distances[i].coords);
 			circuits.splice(index2, 1);
 		}
 		// console.log(circuits);
 	}
-	const uniqueCircuits = circuits.map(circuit => new Set(circuit));
+	const uniqueCircuits = circuits.map((circuit) => new Set(circuit));
 	uniqueCircuits.sort((a, b) => b.size - a.size);
 	// console.log(uniqueCircuits);
 	// console.log(circuits);
@@ -47,12 +65,65 @@ const part1 = (rawInput: string) => {
 		total *= uniqueCircuits[j].size;
 	}
 	return total;
-}
+};
 
 const part2 = (rawInput: string) => {
 	const input = parseInput(rawInput);
-
-	return;
+	let distances: { coords: number[]; distance: number }[] = [];
+	for (let x = 0; x < input.length; x++) {
+		for (let y = x + 1; y < input.length; y++) {
+			distances.push({
+				coords: [x, y],
+				distance: distance(input[x], input[y]),
+			});
+		}
+	}
+	distances.sort((a, b) => a.distance - b.distance);
+	// console.log(distances);
+	let circuits: number[][] = [];
+	let total = 0;
+	for (let i = 0; i < distances.length; i++) {
+		const found = circuits.filter(
+			(circuit) =>
+				circuit.includes(distances[i].coords[0]) ||
+				circuit.includes(distances[i].coords[1]),
+		);
+		if (found.length === 0) {
+			circuits.push(distances[i].coords);
+		}
+		if (found.length === 1) {
+			const index = circuits.findIndex(
+				(circuit) => circuit.join(",") === found[0].join(","),
+			);
+			circuits[index].push(...distances[i].coords);
+		}
+		if (found.length === 2) {
+			const index1 = circuits.findIndex(
+				(circuit) => circuit.join(",") === found[0].join(","),
+			);
+			const index2 = circuits.findIndex(
+				(circuit) => circuit.join(",") === found[1].join(","),
+			);
+			circuits[index1].push(...circuits[index2]);
+			circuits[index1].push(...distances[i].coords);
+			circuits.splice(index2, 1);
+		}
+		// console.log(circuits);
+		if (circuits.length === 1) {
+			const uniqueCircuits = circuits.map((circuit) => new Set(circuit));
+			if (uniqueCircuits[0].size === input.length) {
+				console.log(
+					"All connected with last coords:",
+					distances[i].coords,
+				);
+				total =
+					input[distances[i].coords[0]][0] *
+					input[distances[i].coords[1]][0];
+				break;
+			}
+		}
+	}
+	return total;
 };
 
 run({
